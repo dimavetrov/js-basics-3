c9bca5ee16ae80cbd2721230ad24c55a6ca5603e
dimavetrov <ddvetrov94@gmail.com>
2015-02-04 15:14:02 +0200

    adding the task 3

--- solution.js
+++ solution.js
@@ -1,22 +1,55 @@
 function range(start, end, step) {
   // Write a range function that takes two arguments, start and end,
   // and returns an array containing all the numbers from start up to (and including) end.
+ var arr = [];
+ if(!step){
+  step = 1;
+ }
+  if(step < 0 && start > end){
+   for (var i = start; i > end; i += step)      
+    arr.push(i);
+  }
+
+if (step > 0 ) {
+   for (var i = start; i < end; i += step)
+     arr.push(i);
+ }
+ return arr;
 }
 
 function sum(numbers) {
   // Write a sum function that takes an array of numbers
   // and returns the sum of these numbers.
+  var summ = 0;
+  for(var i=0; i<numbers.length; i++){
+    summ += numbers[i];
+  }
+  return summ;
 }
 
 function reverseArray(arr) {
   // Write a function which takes an array as argument
   // and produces a new array that has the same elements in the inverse order.
+  var arr2 = [];
+  for(var i=0;i<arr.length;i++){
+  arr2.push(arr[i]);
+}
+  arr2.reverse();
+  return arr2;
 }
 
 function reverseArrayInPlace(arr) {
   // Write a function that does what the reverse method does:
   // it modifies the array given as argument in order to reverse
   // its elements. It should not use the standard reverse method.
+  var size = arr.length-1;
+  var temp = 0;
+  for(var i=0;i<size/2; i++){ 
+    temp = arr[i];
+    arr[i] = arr[size-i];
+    arr[size-i] = temp;
+}
+return arr;
 }
 
 function arrayToList(arr) {
@@ -38,24 +71,48 @@ function arrayToList(arr) {
   //   }
   // };
   //
+    
   // Write a function arrayToList that builds up a data structure like
   // the previous one when given [1, 2, 3] as argument. It should use
   // helper function prepend.
+    var size = arr.length;
+    size = size-1;
+    var spisok = null;
+    while (size>=0){
+      spisok = prepend(arr[size--],spisok);
+   }
+    return spisok;
 }
 
 function listToArray(list) {
   // Write a function that produces an array from a list
-}
+  var mass = [];
+  while(list>0){
+    mass.push(list.value);
+    list = list.rest;
+  }
+return mass;
+  }
 
 function prepend(item, list) {
   // Write a function which takes an element and a list and creates a new list
   // that adds the element to the front of the input list.
+   return { value : item, rest: list};
 }
 
 function nth(n, list) {
   // Write which takes a list and a number and returns the element at the
   // given position in the list, or undefined when there is no such element.
   // It should be recursive.
+
+  if(n == 0){
+    return list.value;
+   } 
+    if(list.rest == null){
+      return undefined;
+   }else{
+     return nth(n-1,list.rest);
+  }
 }
 
 function deepEqual(a, b) {
@@ -66,6 +123,37 @@ function deepEqual(a, b) {
   // only if they are the same value or are objects with the same
   // properties whose values are also equal when compared with
   // a recursive call to deepEqual.
+  var property1str = 0;
+ var property2str = 0;
+ var property1 = 0;
+ var property2 = 0;
+ if(a != 'object' && b!= "object" || a == null && b == null){
+  if(a ==b){
+    return true;
+  } else {
+  return false;
+}
+ } if(a == 'object' && b == 'object'){
+    for(property1 in a){
+      property1str++;
+    }
+    for(property2 in b){
+      property2str++;
+    }
+    if(property1str !=property2str){
+      return false;
+    }else{
+      for(property2 in a){
+        if(property1 in b){
+          return deepEqual(a[property1],b[property2]);
+        }else{
+          return false;
+        }
+      }
+    }
+ }else{
+      return false;
+    }   
 }
 
 module.exports = {
